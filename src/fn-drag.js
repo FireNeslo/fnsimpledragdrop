e42027b37edca10d3c3ddd8686b5b2b74f20d284
Tony Sæle <tony@globalit.no>
2015-07-23 15:33:39 +0200

    Free from the tyranny of semicolons

--- src/fn-drag.js
+++ src/fn-drag.js
@@ -2,15 +2,15 @@ directive('fnDrag', function ($rootScope) {
   'use strict';
   return function (scope, el, attrs) {
     el.attr('draggable', true);
-    var on = el[0].addEventListener.bind(el[0])
-      , dragging = {element: el};
+    var on = el[0].addEventListener.bind(el[0]);
+    var dragging = {element: el};
 
     function dragStart(e) {
       dragging.data = scope.$eval(attrs.fnDrag);
       dragging.source = scope.$eval(attrs.fnSource);
-      e.dataTransfer.effectAllowed = "move";
-      e.dataTransfer.setDragImage(el[0], 0, 0);
-      $rootScope.$emit("fn-dragstart", dragging);
+
+      if (e.dataTransfer) e.dataTransfer.effectAllowed = 'move';
+      $rootScope.$emit('fn-dragstart', dragging, e);
     }
 
     function dragEnd() {
--- src/fn-drop.js
+++ src/fn-drop.js
@@ -1,62 +1,62 @@
 directive('fnDrop', function ($rootScope) {
-  'use strict';
-  var dragging, over = null;
+  'use strict'
+  var dragging, over = null
   $rootScope.$on("fn-dragstart", function (event, data) {
-    dragging = data;
-    angular.element(data.element).addClass("fn-dragging");
-  });
+    dragging = data
+    angular.element(data.element).addClass("fn-dragging")
+  })
 
   $rootScope.$on("fn-dragend", function (event, data) {
-    dragging = null;
-    angular.element(data.element).removeClass("fn-dragging");
-  });
+    dragging = null
+    angular.element(data.element).removeClass("fn-dragging")
+  })
   return {
     restrict: 'A',
     link: function (scope, el, attrs, controller) {
-      var id = 0;
-      var on = el[0].addEventListener.bind(el[0]);
+      var id = 0
+      var on = el[0].addEventListener.bind(el[0])
 
       on("dragover", function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-        e.dataTransfer.dropEffect = 'move';
-      });
+        e.stopPropagation()
+        e.preventDefault()
+        e.dataTransfer.dropEffect = 'move'
+      })
       on("dragenter", function (e) {
-        id++;
-        e.stopPropagation();
-        e.preventDefault();
-        el.addClass('fn-over');
-      });
+        id++
+        e.stopPropagation()
+        e.preventDefault()
+        el.addClass('fn-over')
+      })
       on("dragleave", function (e) {
-        id--;
-        e.stopPropagation();
-        e.preventDefault();
+        id--
+        e.stopPropagation()
+        e.preventDefault()
         if (id === 0) {
-          el.removeClass('fn-over');
+          el.removeClass('fn-over')
         }
-      });
+      })
 
       on("drop", function (e) {
-        id = 0;
-        e.preventDefault();
-        el.removeClass('fn-over');
+        id = 0
+        e.preventDefault()
+        el.removeClass('fn-over')
         scope.$apply(function () {
           scope.$eval(attrs.fnDrop, {
             $over: over,
             $data: dragging.data,
             $source: dragging.source,
             $target: scope.$eval(attrs.fnTarget)
-          });
-        });
-      });
+          })
+        })
+      })
     },
     controller: function ($scope) {
       this.over = function (data) {
-        over = data;
-      };
+        over = data
+      }
       this.leave = function (data) {
-        over = null;
-      };
+        over = null
+      }
     }
   }
 })
